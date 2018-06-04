4a00b452442577c6034aa979a322a2beadad3957
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-04 12:49:22 -0300

    arquivo sem print

--- introducao.py
+++ introducao.py
@@ -7,5 +7,4 @@ s = '''Primeiro valor digitado {0:.>10}
 Segundo valor digitado {1:.>11}
 Resultado da soma: {2:.>15}'''
 
-print(s.format(num1, num2, num3))print("Oi Github")
-print("Oi Github")
+print(s.format(num1, num2, num3))
