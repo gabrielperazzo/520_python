a8dd222254da3a567bbfe1e743aaccf9f2c1c269
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-04 15:04:43 -0300

    elif

--- estruturas.py
+++ estruturas.py
@@ -5,10 +5,12 @@
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro número: '))
 
-#if num1 < num2:
-#    s = '{0} é menor que {1}'
-#else:
-#    s = '{0} não é menor que {1}'
-s = '{0} é menor que {1}' if num1 < num2 else '{0} não é menor que {1}'
+if num1 < num2:
+    s = '{0} é menor que {1}'
+elif num1 == num2:
+    s = '{0} é igual a {1}'
+else:
+    s = '{0} não é menor que {1}'
+#s = '{0} é menor que {1}' if num1 < num2 else '{0} não é menor que {1}'
 
 print(s.format(num1,num2))
