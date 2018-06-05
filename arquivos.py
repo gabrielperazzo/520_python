429bdeb27fe905d0ec4748f24cd741dd265e74ed
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-05 17:03:38 -0300

    files2

--- arquivos.py
+++ arquivos.py
@@ -2,9 +2,16 @@
 
 import random
 
-f = open('usuarios.csv' , 'a')
-#linhas = f.readlines()
-#f.close
+f = open('usuarios.csv' , 'r')
+linhas = f.readlines()
+f.close()
 
-nome = input('Digite um nome: ')
-f.write(str(random.randint(0,100)) + ',' + nome + '\n')
+if ',' in linhas[0]:
+    print('O banco de dados já está atualizado')
+    exit()
+    
+f = open('usuarios.csv' , 'w')
+for l in linhas:
+    f.write('{0},{1}'.format(random.randint(0,100),l))
+
+f.close()
