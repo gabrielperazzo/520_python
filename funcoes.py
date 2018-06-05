00bad7c85be74f12466f26412c700ab34d3d3463
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-05 14:16:21 -0300

    args

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def nomeados(a, b='dois', c='tres'):
+    print(a, b, c)
+
+nomeados('um', b='quatro', c='cinco')
+
+def args(*a):
+    print(a[5])
+
+def kwargs(**k):
+    print(k)
+
+#args(1,2,3,4,5,6,7,8,9,0)
+
+kwargs(h='hector', d='danilo', t='thais', b='bruna', j='joyce')
