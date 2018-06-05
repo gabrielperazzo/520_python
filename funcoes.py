a49b471043cdb46fc06393ee4d93154ebdba8a9a
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-05 15:08:44 -0300

    funcoes2

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 
-def nomeados(a, b='dois', c='tres'):
-    print(a, b, c)
+#def nomeados(a, b='dois', c='tres'):
+#   print(a, b, c)
 
-nomeados('um', b='quatro', c='cinco')
+#nomeados('um', b='quatro', c='cinco')
 
 def args(*a):
-    print(a[5])
+    print(a)
 
 def kwargs(**k):
     print(k)
 
+def ambos(*a, **k):
+    print(a)
+    print(k)
+
+tupla = ('Hector' , '123' , '554322', 'hector.vido')
+#a, *b, c = tupla
+#print(a, c)
+
+lista = ['Gatinho', 'Cachorrinho', 'Patinho', 'Leãozinho']
+def escopo():
+    #lista = ['Rosinha', 'Copo-de-leitinho', 'Dente-de-leaozinho']
+    lista.append('Jacarezinho')
+    print(lista)
+
+def externa():
+    print('TEste')
+    def interna():
+        print('Priiiishhh')
+    return interna
+
+externa()()
+    
 #args(1,2,3,4,5,6,7,8,9,0)
 
-kwargs(h='hector', d='danilo', t='thais', b='bruna', j='joyce')
+#kwargs(h='hector', d='danilo', t='thais', b='bruna', j='joyce')
---
+++ teste1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def externa():
+    def interna():
+        print('Priiiishhh')
+    return interna
+
+externa()
+    
