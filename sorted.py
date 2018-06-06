391299d83ec19fc60dc7b5ecf2e7106507e098ef
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-06 09:15:26 -0300

    sorted

--- sorted.py
+++ sorted.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+f = open('usuarios.csv', 'r')
+usuarios = [u.strip().split(',') for u in f.readlines()]
+f.close()
+
+def ordenar_pelo_id(u):
+    u[0] = int(u[0])
+    return u
+
+for u in sorted(usuarios, key=ordenar_pelo_id):
+    print(u)
+
+exit()
+
 f = open('usuarios.csv' , 'r')
 linhas = f.readlines()
 f.close()
@@ -8,5 +21,3 @@ usuarios = []
 for l in sorted(linhas):
     usuarios.append(l.strip().split(','))
     print(usuarios)
-
-    
