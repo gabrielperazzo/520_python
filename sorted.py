9c7269cc6a72b291203de53e253655e386e8a503
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-06 10:23:32 -0300

    sorted4

--- sorted.py
+++ sorted.py
@@ -9,14 +9,14 @@ print(sorted(lista))
 print(sorted(lista, key=minusculizar))
 
 f = open('usuarios.csv', 'r')
-usuarios = [u.strip().split(',') for u in f.readlines()]
+usuarios = [l for l in [u.strip().split(',') for u in f.readlines()] if l[1].lower().startswith('a')] 
 f.close()
 
 def ordenar_pelo_nome(i):
     i[1] = str(i[1])
     return i[1].lower()
 
-for u in sorted(usuarios, key=ordenar_pelo_nome):
+for u in sorted(usuarios, key=lambda i : i[1].lower()):
     print(u)
 
 exit()
