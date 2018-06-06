63efae0f62304ae8e4ac7739d03dce22486f13d3
Gabriel Perazzo <gabrielperazzo@live.com>
2018-06-06 16:09:08 -0300

    classes1

--- classes.py
+++ classes.py
@@ -6,8 +6,15 @@ class Arquivo:
         self.nome = nome
         self.arquivo = open(nome, 'r')
         self.linhas = len(self.arquivo.readlines())
+        self.arquivo.seek(0)
+
+    def ler(self, n):
+        for i, l in enumerate(self.arquivo, start=1):
+            if i == n:
+                return l.strip()
 
 arquivo = Arquivo('usuarios.csv')
 letras = Arquivo('letras.txt')
 print(arquivo.nome, arquivo.linhas)
 print(letras.nome, letras.linhas)
+print(arquivo.ler(12))
